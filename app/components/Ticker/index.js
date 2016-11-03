2a0b138eb3df6ed5da4dec393b948cb117afe1aa
Phil Callister <p.callister@sportradar.com>
2016-11-02 19:51:34 -0500

    Ticker UI Update

--- app/components/Ticker/index.js
+++ app/components/Ticker/index.js
@@ -1,21 +1,33 @@
-import React from "react"
-import cssModules from "react-css-modules"
-import style from "./style.css"
+import React from "react";
+import cssModules from "react-css-modules";
+import classNames from 'classnames/bind';
+import style from "./style.css";
+
+let cx = classNames.bind(style);
 
 export class Ticker extends React.Component {
   constructor(props) {
     super(props);
     this.socket = props.socket;
     this.symbol = props.symbol;
-    this.state = {ticks: []};
+    this.key = 0;
+
+    let fakeTick = {key: this.key, l: "##", c: "##", cp: "##"};
+    this.state = {currentTick: fakeTick, ticks: [fakeTick]};
   }
 
   componentDidMount() {
     let that = this;
     this.channel = this.socket.channel(`symbol:${this.symbol}`);
     this.channel.join();
-    this.channel.on('quote', function(data){
-      that.setState({ticks: that.state.ticks.concat(data)})
+    this.channel.on('quote', function(tick){
+      tick.key = that.state.currentTick.key + 1;
+      let ticks = that.state.ticks;
+      if (ticks.length >= 5) {
+        ticks.pop();
+      }
+      ticks.unshift(tick);
+      that.setState({currentTick: tick, ticks: ticks})
     });
   }
 
@@ -24,15 +36,44 @@ export class Ticker extends React.Component {
   }
 
   render() {
-    let tickList = this.state.ticks.map(function(tick, i){
-        return(<li key={i}>{tick.lt} => {tick.c}</li>);
+    let tickList = this.state.ticks.map(function(tick){
+      
+      if (tick.key != 0){
+        return(<li key={tick.key}>{tick.lt}: {tick.l} &rArr; {tick.c} ({tick.cp}%)</li>);
+      }
+      return null
     });
     return (
-        <div className={style.ticker}>
-          <h3 className={style.header}>{this.symbol}</h3>
-          <ul>{tickList}</ul>
+      <div className={style.ticker}>
+        <div className={this.headerClass()}>
+          <div>
+            <h3 className={style.symbol}>{this.symbol}</h3>
+            <span className={style.value}>{this.state.currentTick.l}</span>
+          </div>
+          <div className={style.change}>
+            <span className={style.value}>{this.state.currentTick.c}</span>
+            <span className={style.value}>({this.state.currentTick.cp}%)</span>
+          </div>
         </div>
+        <ul>{tickList}</ul>
+      </div>
     )
+  }
+
+  headerClass() {
+    let change = this.detectChange();
+    return cx({
+      header: true,
+      noChange: change == null,
+      plusChange: change == '+',
+      minusChange: change == '-'
+    });
+  }
+
+  detectChange() {
+    let tick = this.state.currentTick;
+    let tickChange = tick.c.charAt(0);
+    return (tickChange == '-' || tickChange == '+') ? tickChange : null
   }
 }
 
